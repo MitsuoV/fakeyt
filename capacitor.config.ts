1a79ffa34c0345d080f070357d05ddb0f8850d82
MitsuoV <105979406+MitsuoV@users.noreply.github.com>
2026-09-12 16:16:29 +0800

    Add Capacitor app configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.mitsuo.lowkey',
+  appName: "Mitsuo's Corner",
+  webDir: 'outputs',
+  bundledWebRuntime: false,
+  ios: {
+    contentInset: 'automatic'
+  }
+};
+
+export default config;
+
